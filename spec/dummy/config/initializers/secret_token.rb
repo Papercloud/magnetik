f7bed62003543b4921fb896ac510eb1d3e0fa38a
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-05-22 14:15:58 +1000

    Added a secret token to the dummy app to appease Rails 4.0.

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '0a2ff1654a4da13ecda7a43f3a9f3be6be136e840d8e41ce531ec3caa33d9ded61c46a2f8158bf2b346f76bc1ff31d27d1c2d9f98d8be6327830bb8285631e87'
