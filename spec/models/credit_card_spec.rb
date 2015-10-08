606bf88254569fc6e20922800155cebb7d50d082
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-10-08 14:25:22 +1100

    Adds a `requires_validation?` method to credit cards that returns a bool based on the last validation time
    If the card was validated past the set `validation_interval` setting, it will return true, at which point a new pre-auth should occur to re-validate the card. Otherwise false.
    
    It will also return false if the `validation_interval` is set to `nil`.

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -20,5 +20,43 @@ module Magnetik
       it { should validate_presence_of(:brand) }
       it { should validate_length_of(:name).is_at_most(Magnetik.max_name_length) }
     end
+
+    describe '#requires_revalidation?' do
+      before do
+        @credit_card = create(:credit_card)
+      end
+
+      after do
+        Magnetik.validation_interval = 3.months
+      end
+
+      context 'with interval set' do
+        before do
+          Magnetik.validation_interval = 3.months
+        end
+
+        it 'returns false if the credit cards last validation is within the interval' do
+          @credit_card.last_validated_at = 2.months.ago
+
+          expect(@credit_card.requires_revalidation?).to eq false
+        end
+
+        it 'returns true if the credit cards last validation is outside the interval' do
+          @credit_card.last_validated_at = 4.months.ago
+
+          expect(@credit_card.requires_revalidation?).to eq true
+        end
+      end
+
+      context 'without interval set' do
+        before do
+          Magnetik.validation_interval = nil
+        end
+
+        it 'returns false' do
+          expect(@credit_card.requires_revalidation?).to eq false
+        end
+      end
+    end
   end
 end
